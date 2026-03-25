2109c669d5560880d9c2c4f6395766f4fc715df8
Vikram Hegde <vikramhegde4037@gmail.com>
2026-03-25 17:54:59 +0530

    Added backup api

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,20 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
 import { PGProvider } from './context/PGContext.tsx'
 import { ThemeProvider } from './context/ThemeContext.tsx'
+import { initApiBase } from './lib/api.ts'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <PGProvider>
-        <AuthProvider>
-          <ThemeProvider>
-            <App />
-          </ThemeProvider>
-        </AuthProvider>
-      </PGProvider>
-    </BrowserRouter>
-  </StrictMode>,
-)
+initApiBase().finally(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <BrowserRouter>
+        <PGProvider>
+          <AuthProvider>
+            <ThemeProvider>
+              <App />
+            </ThemeProvider>
+          </AuthProvider>
+        </PGProvider>
+      </BrowserRouter>
+    </StrictMode>,
+  )
+});
